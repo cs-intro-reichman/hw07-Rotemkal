27750af2d1f9cc99ed4a676dc309e8743ac0cade
רותם קליר <rotem.kalir@post.runi.ac.il>
2024-01-30 21:58:10 +0200

    hw07

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,25 +11,51 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
-	}
+		return str.substring(1);
+	} 
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		if (word1.length()==0){
+			return word2.length();
+		}
+		if (word2.length()==0){
+			return word1.length();
+		}
+		if (word1.charAt(0)==word2.charAt(0)){
+			return levenshtein(tail(word1), tail(word2));
+		}
+		else {
+			return 1 + Math.min(Math.min((levenshtein(tail(word1),word2)),(levenshtein(tail(word2),word1))),(levenshtein(tail(word1),tail(word2))));
+		}
 	}
 
 	public static String[] readDictionary(String fileName) {
-		String[] dictionary = new String[3000];
-
-		In in = new In(fileName);
-
-		// Your code here
-
-		return dictionary;
-	}
+			String[] dictionary = new String[3000];
+			In in = new In(fileName);
+			for (int i = 0 ; i < 3000 ; i++){
+				String word = in.readLine();
+				dictionary[i] = word;
+			}
+			return dictionary;
+		}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		String similar = "";
+		int min = threshold + 1, diffrence ; 
+		for (int i = 0 ; i < 3000 ; i++)
+		{
+			diffrence = levenshtein(dictionary[i],word);
+			if (diffrence < min)
+			{
+				similar = dictionary[i];
+				min = diffrence;
+			}
+		}
+		if (min <= threshold)
+		{
+		return similar;
+		}
+		return word;
 	}
 
 }
